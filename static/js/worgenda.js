6bf2035b31f60eb3ad03d50946901fc1b546e62e
Sergio de Mingo <sdemingo@gmail.com>
2016-02-20 11:36:52 +0100

    add static files

---
+++ static/js/worgenda.js
@@ -0,0 +1,35 @@
+
+
+var W={}
+
+$(document).ready(function(){
+
+    W.Events=[]
+    
+    $("#datepicker").datepicker({
+	dayNames: [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" ],
+	dayNamesMin: [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa" ],
+	dayNamesShort: [ "Dom", "Lun", "Mar", "Mier", "Jue", "Vie", "Sab" ],
+	monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+		      "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+	firstDay: 1
+    })
+
+    getEvents()
+})
+
+
+function getEvents(){
+
+    $.ajax({
+    	url:"/events",
+    	type: 'get',
+	dataType: 'json',
+    	success: function (events){
+	    console.log(events)
+	},
+    	error: function(error){
+	    console.log(error)
+	}
+    })
+}
